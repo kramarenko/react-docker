8bafc5120cb2e906f5b45485dd8abe11f99943ec
kramarenko <vasil@kramarenko.com.ua>
2021-08-04 00:29:08 +0300

    add auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { Calendar } from './Calendar';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
-import { OktaAuth } from '@okta/okta-auth-js';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppWithRouterAccess from './AppWithRouterAccess';
 
-function App() {
+const App = () => {
 	return (
 		<Router>
-			<Security
-				issuer='https://dev-79232118.okta.com/oauth2/default'
-				clientId='0oa1egxj6bYexox5A5d7'
-				redirectUri={window.location.origin + '/callback'}
-				pkce={true}
-			>
-				<SecureRoute path='/' exact={true} component={Calendar} />
-				<Route path='/callback' component={LoginCallback} />
-			</Security>
+			<AppWithRouterAccess />
 		</Router>
 	);
-}
+};
 
 export default App;
